74d8bcdfb78f0dd459ccc7ea1f9f262635e37783
Kevin Phillips <phillipskevin386@gmail.com>
2024-06-25 23:02:00 -0400

    add callback to showDialog to return user to home screen; bug fix on npe for non digit response

--- app/src/main/java/org/brightmindenrichment/street_care/util/Extensions.kt
+++ app/src/main/java/org/brightmindenrichment/street_care/util/Extensions.kt
@@ -911,7 +911,8 @@ class Extensions {
             title: String,
             message: String,
             textPositive: String,
-            Cancel: String
+            Cancel: String,
+            callback: () -> Unit = {}
         ){
             val builder = AlertDialog.Builder(context)
             builder.setTitle(title)
@@ -923,6 +924,7 @@ class Extensions {
                 )
             val alert = builder.create()
             alert.show()
+            callback()
         }
 
         fun dateParser(date: String): LocalDateTime? {
