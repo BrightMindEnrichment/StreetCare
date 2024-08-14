fa9dcf9b4280346eb4d06c48cc4f778023311cbb
Kevin Phillips <phillipskevin386@gmail.com>
2024-08-14 19:48:58 -0400

    remove firebase play services library for coroutine support; instead add custom Task ext

--- app/src/main/java/org/brightmindenrichment/street_care/ui/user/UserRepository.kt
+++ app/src/main/java/org/brightmindenrichment/street_care/ui/user/UserRepository.kt
@@ -1,53 +1,70 @@
 package org.brightmindenrichment.street_care.ui.user
 
+
 import android.content.ContentValues
 import android.util.Log
-import com.google.firebase.auth.ktx.auth
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.tasks.await
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class UserRepository {
+
     suspend fun fetchUserData(): UserModel {
         val userModel = UserModel()
-        val currentUser = Firebase.auth.currentUser
+        val currentUser = FirebaseAuth.getInstance().currentUser
         userModel.currentUser = currentUser
-        val storageRef = Firebase.storage.reference
+        val storageRef = FirebaseStorage.getInstance().reference
 
         if (currentUser == null) {
             Log.d(ContentValues.TAG, "No user")
             return userModel
         }
 
-        Log.d(ContentValues.TAG, "getUserData")
-        val db = FirebaseFirestore.getInstance()
-        val docRef = db.collection("users").document(currentUser.uid)
-
         try {
-            val document = docRef.get().await()
-            if (document != null && document.data != null) {
-                val user = document.data!!
-                userModel.userName =
-                    user["username"]?.toString() ?: currentUser.displayName.toString()
-                Log.d(
-                    "ContentValues",
-                    "UserSingleton.userModel: suspend :: ${UserSingleton.userModel}"
-                )
+            val db = FirebaseFirestore.getInstance()
+            val docRef = db.collection("users").document(currentUser.uid)
+            val document: DocumentSnapshot = docRef.get().awaitTask()
+
+            if (document.exists()) {
+                val user = document.data
+                if (!user.isNullOrEmpty()) {
+                    userModel.userName = user["username"]?.toString() ?: currentUser.displayName.toString()
+                    Log.d(ContentValues.TAG, "UserSingleton.userModel: suspend :: ${UserSingleton.userModel}")
+                } else {
+                    Log.d(ContentValues.TAG, "Document is empty")
+                }
             } else {
-                Log.d(ContentValues.TAG, "No such document user")
+                Log.d(ContentValues.TAG, "No such document")
             }
 
             val fileName = "profile.jpg"
-            val imageRef = storageRef.child("users").child(currentUser.uid).child(fileName)
-            val uri = imageRef.downloadUrl.await()
+            val imageRef = storageRef.child("users/${currentUser.uid}/$fileName")
+            val uri = imageRef.downloadUrl.awaitTask()
             userModel.imageUri = uri.toString()
+
             Log.d(ContentValues.TAG, "Get image: success")
         } catch (e: Exception) {
-            Log.d(ContentValues.TAG, "Fetch user data failed with ", e)
+            Log.e(ContentValues.TAG, "Error fetching user data", e)
         }
 
-        Log.d(ContentValues.TAG, "fetchUserData finish: " + userModel.userName)
+        Log.d(ContentValues.TAG, "fetchUserData finish: ${userModel.userName}")
         return userModel
+    }
+}
+
+suspend fun <T> Task<T>.awaitTask(): T {
+    return suspendCoroutine { continuation ->
+        this.addOnSuccessListener { result ->
+            continuation.resume(result)
+        }
+        this.addOnFailureListener { exception ->
+            continuation.resumeWithException(exception)
+        }
     }
 }
