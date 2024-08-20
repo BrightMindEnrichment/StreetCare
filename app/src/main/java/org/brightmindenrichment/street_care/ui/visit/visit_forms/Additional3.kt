dfe542ed26d35cef0d5ea5b1b14b3e35daf40047
swathi <sswathika99@gmail.com>
2024-08-20 08:40:27 -0400

    Highlighting the selected option

--- app/src/main/java/org/brightmindenrichment/street_care/ui/visit/visit_forms/Additional3.kt
+++ app/src/main/java/org/brightmindenrichment/street_care/ui/visit/visit_forms/Additional3.kt
@@ -43,7 +43,7 @@ class Additional3 : Fragment() {
         binding.btnMaybe.setBackgroundColor(Color.TRANSPARENT)
 
         binding.btnYes.setOnClickListener {
-            binding.btnYes.setBackgroundColor(Color.TRANSPARENT)
+            binding.btnYes.setBackgroundColor(Color.LTGRAY)
             binding.btnNo.setBackgroundColor(Color.TRANSPARENT)
             binding.btnMaybe.setBackgroundColor(Color.TRANSPARENT)
             sharedVisitViewModel.visitLog.visitAgain = "Yes"
@@ -51,12 +51,12 @@ class Additional3 : Fragment() {
 
         binding.btnNo.setOnClickListener {
             sharedVisitViewModel.visitLog.visitAgain = "No"
-            binding.btnNo.setBackgroundColor(Color.TRANSPARENT)
+            binding.btnNo.setBackgroundColor(Color.LTGRAY)
             binding.btnYes.setBackgroundColor(Color.TRANSPARENT)
             binding.btnMaybe.setBackgroundColor(Color.TRANSPARENT)
         }
         binding.btnMaybe.setOnClickListener {
-            binding.btnMaybe.setBackgroundColor(Color.TRANSPARENT)
+            binding.btnMaybe.setBackgroundColor(Color.LTGRAY)
             binding.btnNo.setBackgroundColor(Color.TRANSPARENT)
             binding.btnYes.setBackgroundColor(Color.TRANSPARENT)
             sharedVisitViewModel.visitLog.visitAgain = "MayBe"
