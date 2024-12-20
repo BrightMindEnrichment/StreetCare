9ef215bf3be38d1d6b2278d65cf819e670de4510
TrilokPalla <p.trilok9@gmail.com>
2024-12-20 17:50:19 -0500

    Added "Share with Community" dialog box to the Visit log page

--- app/src/main/java/org/brightmindenrichment/street_care/ui/visit/visit_forms/SurveySubmittedFragment.kt
+++ app/src/main/java/org/brightmindenrichment/street_care/ui/visit/visit_forms/SurveySubmittedFragment.kt
@@ -1,5 +1,6 @@
 package org.brightmindenrichment.street_care.ui.visit.visit_forms
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +11,13 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import org.brightmindenrichment.street_care.R
 import org.brightmindenrichment.street_care.databinding.FragmentSurvaySubmittedBinding
+import android.widget.Toast
+import android.util.Log
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import android.content.ContentValues.TAG
+import org.brightmindenrichment.street_care.ui.visit.data.VisitLog
 import org.brightmindenrichment.street_care.util.Extensions
 
 
@@ -31,6 +39,10 @@ class SurveySubmittedFragment : Fragment() {
         binding.btnAnotherVisit.setOnClickListener{
             findNavController().navigate(R.id.action_surveySubmittedFragment_to_visitFormFragment1)
         }
+        binding.btnShare.setOnClickListener{
+//            findNavController().navigate(R.id.surveySubmittedFragment)
+            showSharePopup()
+        }
         binding.btnReturnHome.setOnClickListener{
             findNavController().navigate(R.id.action_surveySubmittedFragment_to_nav_home)
         }
@@ -42,6 +54,91 @@ class SurveySubmittedFragment : Fragment() {
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
     }
+
+    // Function to show the share confirmation popup
+    private fun showSharePopup() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("The following information will be shared when posted to the community:")
+            .setMessage(
+                """
+                - Your Name
+                - Your Profile Picture
+                - Your Location
+                - Type of Help Provided
+                """.trimIndent()
+            )
+            .setPositiveButton("Confirm") { dialog, _ ->
+                // Logic for confirming the share action
+                saveTopersonalVisitLog()
+
+               // Toast.makeText(context, "Info Shared Successfully", Toast.LENGTH_SHORT).show()
+
+                // Reset the visit log for future use
+                sharedVisitViewModel.resetVisitLogPage()
+
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                // Simply dismiss the popup
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+
+    // Function to save the visit log details to Firebase "PersonalVisitLog"
+    private fun saveTopersonalVisitLog() {
+        val user = Firebase.auth.currentUser
+        val db = Firebase.firestore
+
+        // Ensure a user is logged in
+        if (user != null) {
+            // Retrieve the current visitLog from ViewModel
+            val visitLog = sharedVisitViewModel.visitLog
+
+             //Prepare the data to save
+            val visitData = hashMapOf(
+                "HelpTime" to visitLog.helpTime,
+                "dateTime" to visitLog.date,
+                "whenVisitTime" to visitLog.whenVisitTime,
+                "NumberOfPeopleHelped" to visitLog.peopleCount,
+                "PeopleHelpedDescription" to visitLog.names,
+                "rating" to visitLog.experience,
+                "share" to visitLog.share,
+                "uid" to user.uid,
+                "number_of_items_donated" to visitLog.number_of_items,
+                "WhatGiven" to visitLog.whattogive,
+                "Location" to visitLog.locationmap,
+                "Type" to visitLog.typeofdevice,
+                "food_drink" to visitLog.food_drink,
+                "clothes" to visitLog.clothes,
+                "hygine" to visitLog.hygine,
+                "wellness" to visitLog.wellness,
+                "lawyerLegal" to visitLog.lawyerLegal,
+                "medicalhelp" to visitLog.medicalhelp,
+                "social" to visitLog.socialWorker,
+                "other" to visitLog.other
+//                //"public" to true,
+            )
+
+            // Save data to the "PersonalVisitLog" collection
+            db.collection("personalVisitLog")
+                .add(visitData)
+                .addOnSuccessListener {
+                    Log.d(TAG, "Visit Log successfully shared to PersonalVisitLog")
+                    Toast.makeText(context, "Information Shared Successfully", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Log.e(TAG, "Error sharing Visit Log: ", e)
+                    Toast.makeText(context, "Failed to share info", Toast.LENGTH_SHORT).show()
+                }
+        } else {
+            Log.e(TAG, "User is not logged in")
+            Toast.makeText(context, "Please log in to share info", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
