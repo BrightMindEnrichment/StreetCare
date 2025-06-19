f77d83ae704ef95d4052fb4bef8523a28ea4e219
Vishal Reddy Musku <61424506+VishalMusku@users.noreply.github.com>
2025-06-19 00:22:11 -0500

    Interaction log editing new logs only (#183)
    * Added 13 Questions to view the log details, share with community button from new, old-iOS,Android collections
    
    * Added the text translations for Spanish strings.xml file
    
    * Added files related to edit feature from edit_log_view branch
    
    * Added editing feature to update Interaction Logs & share with community
    
    * Fixed the edit to one interaction question resetting other questions info.
    
    * Editing Feature For Interaction Logs limited to New Logs Only.
    
    * Editing Feature For Interaction Logs limited to New Logs Only -added toast messgaes.
    
    * Removed skip, removed NA values, stopped writing to webprod, fixed fragment 2 bug which is not saving city and state manual entries.
    
    * Fixed lastedited key, added new popup changes, tried to resolve some conflicts.
    
    * Editing Feature For Interaction Logs limited to New Logs Only -added toast messgaes.
    
    * updated strings.xml after removing duplicate strings.
    
    ---------
    
    Co-authored-by: vishalreddy <Vishal.Reddy.Musku-1@ou.edu>

--- app/src/main/java/org/brightmindenrichment/street_care/ui/visit/visit_forms/SurveySubmittedFragment.kt
+++ app/src/main/java/org/brightmindenrichment/street_care/ui/visit/visit_forms/SurveySubmittedFragment.kt
@@ -27,6 +27,7 @@ class SurveySubmittedFragment : Fragment() {
     private val binding get() = _binding!!
     private var clicked = false
     private var sharedCommunity = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,43 +37,33 @@ class SurveySubmittedFragment : Fragment() {
         return binding.root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnAnotherVisit.setOnClickListener{
+        binding.btnAnotherVisit.setOnClickListener {
             sharedCommunity = false
             clicked = false
+            sharedVisitViewModel.resetVisitLogPage() // Add this to clear old visit data
             findNavController().navigate(R.id.action_surveySubmittedFragment_to_visitFormFragment2)
         }
+
+
         binding.btnShare.setOnClickListener{
-//            findNavController().navigate(R.id.surveySubmittedFragment)
-          //  showSharePopup()
+//            showSharePopup()
             sharedCommunity  =true
             clicked = false
             showCustomDialogForSC()
+
         }
         binding.btnReturnHome.setOnClickListener{
-            clicked =true
-            sharedCommunity = false
-            findNavController().navigate(R.id.action_surveySubmittedFragment_to_nav_visit)
+            findNavController().navigate(R.id.action_surveySubmittedFragment_to_nav_home)
         }
         // Handle back button press
-       val callback = object : OnBackPressedCallback(true) {
+        val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                clicked =true
                 findNavController().navigate(R.id.action_surveySubmittedFragment_to_nav_visit)
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
-
-
-
-
-
     }
 
     // Function to show the share confirmation popup
@@ -86,9 +77,9 @@ class SurveySubmittedFragment : Fragment() {
                 //update the isPublic to true
                 val docId = sharedVisitViewModel.visitLog.documentId
                 if (docId != null) {
-                    updateVisitLogField(docId, "isPublic", true)
+                    updateVisitLogField(docId)
                 }
-                saveVisitLog()
+               // saveVisitLog()
                 // Reset the visit log for future use
                 sharedVisitViewModel.resetVisitLogPage()
                 dialog.dismiss()
@@ -101,19 +92,34 @@ class SurveySubmittedFragment : Fragment() {
             .show()
     }
 
-    fun updateVisitLogField(documentId: String, fieldName: String, newValue: Any) {
+
+    fun updateVisitLogField(documentId: String) {
         val user = Firebase.auth.currentUser ?: return
-        Log.d("BME", user.uid)
         val db = Firebase.firestore
-        val docRef = db.collection("VisitLogBook_New").document(documentId)
-        docRef.update(fieldName, newValue)
-            .addOnSuccessListener {
-                Log.d("Firestore", "Field updated successfully")
+
+        db.collection("users").document(user.uid).get()
+            .addOnSuccessListener { document ->
+                val userType = document.getString("Type") ?: ""
+                val status = if (userType == "Chapter Leader" || userType == "Street Care Hub Leader") "approved" else "pending"
+
+                val updateMap = mapOf(
+                    "isPublic" to true,
+                    "status" to status
+                )
+
+                db.collection("VisitLogBook_New").document(documentId).update(updateMap)
+                    .addOnSuccessListener {
+                        Log.d("Firestore", "Interaction Log published.")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("Firestore", "Failed to publish.", e)
+                    }
             }
             .addOnFailureListener { e ->
-                Log.w("Firestore", "Error updating field", e)
+                Log.w("Firestore", "Failed to retrieve user type", e)
             }
     }
+
 
     fun showCustomDialogForSC() {
         val dialogView = layoutInflater.inflate(R.layout.dialog_popup_shared_comm_visit_log, null)
@@ -131,10 +137,21 @@ class SurveySubmittedFragment : Fragment() {
 
         btnOK.setOnClickListener {
             // Logic for confirming the share action
-            saveVisitLog()
+//            saveVisitLog()
+            // Reset the visit log for future use
+            sharedVisitViewModel.visitLog.isPublic = true
+            //update the isPublic to true
+            val docId = sharedVisitViewModel.visitLog.documentId
+            if (docId != null) {
+                updateVisitLogField(docId)
+            }
+            // saveVisitLog()
             // Reset the visit log for future use
             sharedVisitViewModel.resetVisitLogPage()
-            findNavController().navigate(R.id.action_surveySubmittedFragment_to_sharedCommunityVisitLogFragment)
+
+//            findNavController().navigate(R.id.action_surveySubmittedFragment_to_sharedCommunityVisitLogFragment)
+            Toast.makeText(requireContext(), "Interaction log published.", Toast.LENGTH_SHORT).show()
+
             dialog.dismiss()
 
         }
@@ -142,19 +159,12 @@ class SurveySubmittedFragment : Fragment() {
             dialog.dismiss()
         }
 
-
-
-
         dialog.show()
-
-        dialog.window?.setLayout(
-            (resources.displayMetrics.widthPixels * 0.80).toInt(), // 85% of screen width
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
     }
 
 
-    // Function to save the visit log details to Firebase "PersonalVisitLog"
+
+        // Function to save the visit log details to Firebase "PersonalVisitLog"
     private fun saveVisitLog() {
         val user = Firebase.auth.currentUser
         val db = Firebase.firestore
@@ -174,60 +184,34 @@ class SurveySubmittedFragment : Fragment() {
                     val status =
                         if (userType == "Chapter Leader" || userType == "Street Care Hub Leader") "approved" else "pending"
 
-                    //Prepare the data to save
+                    val visit = visitLog.whereVisit ?: ""
+                    val parts = visit.split(",").map { it.trim() }
+
+                    val street = parts.getOrNull(0) ?: ""
+                    val city = parts.getOrNull(1) ?: ""
+                    val state = parts.getOrNull(2) ?: ""
+                    val zipcode = parts.getOrNull(3) ?: ""
+
+
                     val visitData = hashMapOf(
-                        "whenVisit" to visitLog.date, //1
-                        //"whenVisitTime" to visitLog.whenVisitTime, //1
-                        "whereVisit" to visitLog.whereVisit, //2
-                        //"Location" to visitLog.locationmap, //2
-                        "locationDescription" to visitLog.locationDescription, //2
-                        "peopleHelped" to visitLog.peopleCount, //3
-                        "peopleHelpedDescription" to visitLog.names, //3
-                        "foodAndDrinks" to visitLog.food_drink, //4
-                        "clothes" to visitLog.clothes, //4
-                        "hygiene" to visitLog.hygiene, //4
-                        "wellness" to visitLog.wellness, //4
-                        "medical" to visitLog.medicalhelp, //4
-                        "social" to visitLog.socialWorker, //4
-                        "legal" to visitLog.lawyerLegal, //4
-                        "other" to visitLog.other, //4
-                        "whatGiven" to visitLog.whattogive, //4
-                        "otherNotes" to visitLog.otherDetail, //4
-                        "itemQty" to visitLog.number_of_items, //5
-                        "itemQtyDescription" to visitLog.itemQtyDescription, //5
-                        "rating" to visitLog.experience, //6
-                        "ratingNotes" to visitLog.comments, //6
-
-                        "durationHours" to visitLog.visitedHours, //1A
-                        "durationMinutes" to visitLog.visitedMinutes, //1A
-                        "numberOfHelpers" to visitLog.whoJoined, //2A
-                        "numberOfHelpersComment" to visitLog.numberOfHelpersComment, //2A
-                        "peopleNeedFurtherHelp" to visitLog.stillNeedSupport, //3A
-                        "peopleNeedFurtherHelpComment" to visitLog.supportTypeNeeded, //3A
-                        "peopleNeedFurtherHelpLocation" to visitLog.peopleNeedFurtherHelpLocation, //3A
-                        "furtherFoodAndDrinks" to visitLog.add_food_drink, //4A
-                        "furtherClothes" to visitLog.add_clothes, //4A
-                        "furtherHygiene" to visitLog.add_hygine, //4A
-                        "furtherWellness" to visitLog.add_wellness, //4A
-                        "furtherMedical" to visitLog.add_medicalhelp, //4A
-                        "furtherSocial" to visitLog.add_socialWorker, //4A
-                        "furtherLegal" to visitLog.add_lawyerLegal, //4A
-                        "furtherOther" to visitLog.add_other, //4A
-                        "furtherOtherNotes" to visitLog.add_otherDetail, //4A
-                        "whatGivenFurther" to visitLog.whatrequired, //4A
-                        "followUpWhenVisit" to visitLog.followupDate, //5A
-                        "futureNotes" to visitLog.futureNotes, //6A
-                        "volunteerAgain" to visitLog.visitAgain, //7A
-
-                        "lastEdited" to visitLog.lastEditedTime,
-                        "type" to visitLog.typeofdevice,
-                        "timeStamp" to visitLog.createdTime,
                         "uid" to user.uid,
-                        "isPublic" to visitLog.share,
-                        "isFlagged" to visitLog.isFlagged,
-                        "flaggedByUser" to visitLog.flaggedByUser
-//                //"public" to true,
+                        "dateTime" to visitLog.date,
+                        "state" to state,
+                        "city" to city,
+                        "stateAbbv" to state,
+                        "street" to street,
+                        "zipcode" to zipcode,
+                        "numberPeopleHelped" to visitLog.peopleCount,
+                        "whatGiven" to visitLog.whattogive,
+                        "public" to "true",
+                        "status" to status,
+                        "itemQty" to visitLog.number_of_items,
+                        "rating" to visitLog.experience,
+                        "Description" to visitLog.peopleHelpedDescription,
+                        "flaggedByUser" to visitLog.flaggedByUser,
+                        "isFlagged" to visitLog.isFlagged
                     )
+
 
                     // Save data to the "visitLogWebProd" collection
                     db.collection("visitLogWebProd")
@@ -266,22 +250,19 @@ class SurveySubmittedFragment : Fragment() {
         super.onDestroy()
         _binding = null
     }
+
+
     override fun onDestroyView() {
         super.onDestroyView()
 
+        // Only redirect if user clicked "Return Home" or "Back"
         if (clicked && !sharedCommunity) {
-
             requireActivity()
                 .findViewById<BottomNavigationView>(R.id.bottomNav)
                 .selectedItemId = R.id.loginRedirectFragment
-            clicked = false
         }
-     /*   } else if (!clicked && !sharedCommunity) {
-            requireActivity()
-                .findViewById<BottomNavigationView>(R.id.bottomNav)
-                .selectedItemId = R.id.loginRedirectFragment
-        } else {
 
-        }*/
+        _binding = null
     }
+
 }// end of class
